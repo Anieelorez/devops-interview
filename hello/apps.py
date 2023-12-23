efa55e6731cadabe84bd41660563d2447e32b663
anieelorez <anieelorez1@gmail.com>
2023-12-22 23:10:26 -0800

    update

--- hello/apps.py
+++ hello/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InterviewConfig(AppConfig):
+class HelloConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'interview'
+    name = 'hello'
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("interview.urls")),
+    path("hello", include("hello.urls")),
+    path("health", include("hello.urls"))
 ]
+
